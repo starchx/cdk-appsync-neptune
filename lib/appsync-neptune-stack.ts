068d876f967d860abbd4efd9966f2285330a3421
Xin Chen <xinchx@amazon.com>
2023-07-25 12:37:56 +1000

    feat: added adot auto instrument

--- lib/appsync-neptune-stack.ts
+++ lib/appsync-neptune-stack.ts
@@ -20,15 +20,21 @@ export class AppsyncNeptuneStack extends cdk.Stack {
     })
 
     const vpc = new ec2.Vpc(this, 'NewNeptuneVPC');
-
+    const region = cdk.Stack.of(this).region
     const lambdaFn = new lambda.Function(this, 'Lambda Function', {
-     runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'main.handler',
       code: lambda.Code.fromAsset('lambda-fns'),
       memorySize: 1024,
-      vpc
+      vpc,
+      // This adds and enables ADOT auto instrument
+      tracing: cdk.aws_lambda.Tracing.ACTIVE,
+      layers: [lambda.LayerVersion.fromLayerVersionArn(this, 'LambdaLayer',
+        `arn:aws:lambda:${region}:901920570463:layer:aws-otel-nodejs-amd64-ver-1-13-0:2`
+      )]
     })
-    
+    lambdaFn.addEnvironment('AWS_LAMBDA_EXEC_WRAPPER', '/opt/otel-handler')
+
     // set the new Lambda function as a data source for the AppSync API
     const lambdaDs = api.addLambdaDataSource('lambdaDatasource', lambdaFn);
 
@@ -41,28 +47,31 @@ export class AppsyncNeptuneStack extends cdk.Stack {
       fieldName: "createPost"
     })
 
+    // setup neptune cluster
+    const clusterSecurityGroup = new ec2.SecurityGroup(this, 'ClusterSecurityGroup', {
+      vpc,
+      description: 'Allow neptune access',
+      allowAllOutbound: true,
+      disableInlineRules: true
+    });
+    clusterSecurityGroup.addIngressRule(ec2.Peer.ipv4(vpc.vpcCidrBlock), ec2.Port.tcp(8182), 'allow neptune access from vpc');
     const cluster = new neptune.DatabaseCluster(this, 'NeptuneCluster', {
       vpc,
-      instanceType: neptune.InstanceType.R5_LARGE
+      instanceType: neptune.InstanceType.R5_LARGE,
+      securityGroups: [clusterSecurityGroup]
     })
 
-    cluster.connections.allowDefaultPortFromAnyIpv4('Open to the world')
-
     const writeAddress = cluster.clusterEndpoint.socketAddress;
-
-    new cdk.CfnOutput(this, 'writeaddress', {
+    new cdk.CfnOutput(this, 'WriteAddress', {
       value: writeAddress
     })
 
     const readAddress = cluster.clusterReadEndpoint.socketAddress
-
-    new cdk.CfnOutput(this, 'readaddress', {
+    new cdk.CfnOutput(this, 'ReadAddress', {
       value: readAddress
     })
 
     lambdaFn.addEnvironment('WRITER', writeAddress)
     lambdaFn.addEnvironment('READER', readAddress)
-
   }
 }
-
